6f1f35e0ee29854545b0d9dd8d165996f69b8b36
WAGayashan <dishanchalana1999@gmail.com>
2023-05-11 18:45:25 +0530

    Active tasks and Completed task sections added
    separate 2 sections Active tasks and completed tasks

--- taskr/src/App.tsx
+++ taskr/src/App.tsx
@@ -1,38 +1,47 @@
-import React, {useRef, useState} from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
 import { Todo } from './model';
 import TodoList from './components/TodoList';
+import{ DragDropContext } from 'react-beautiful-dnd'
 
 
 
 
 
-const App:React.FC = () => {
+const App: React.FC = () => {
 
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
 
-  const handleAdd = (e: React.FormEvent) =>{ //create todo
+  const [compleatedTodos, setcompleatedTodos] = useState<Todo[]>([])  //compleated todo state
+
+  const handleAdd = (e: React.FormEvent) => { //create todo
     e.preventDefault();
 
-    if(todo) {
-      setTodos([...todos, {id:Date.now(),todo,isDone:false}]);
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
       setTodo(""); //empty text field after enter new todo
     }
   };
 
   console.log(todos);
-  
+
 
   return (
-    <div className="App">
-      <span className='heading'> Taskr</span>
-      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-      <TodoList todos={todos} setTodos={setTodos}/>
+    <DragDropContext onDragEnd={()=>{}}>
+      <div className="App">
+        <span className='heading'> Taskr</span>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+        <TodoList todos={todos} setTodos={setTodos}
+          compleatedTodos={compleatedTodos}
+          setcompleatedTodos={setcompleatedTodos}
+        />
 
-      
-    </div>
+      </div>
+
+    </DragDropContext>
+
   );
 }
 
--- taskr/src/components/TodoList.tsx
+++ taskr/src/components/TodoList.tsx
@@ -2,29 +2,59 @@ import React from 'react'
 import './styles.css';
 import { Todo } from '../model';
 import SingleTodo from './SingleTodo';
+import { Droppable } from 'react-beautiful-dnd';
 
 
 
 interface Props {
     todos: Todo[];
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+    compleatedTodos: Todo[];
+    setcompleatedTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const TodoList: React.FC<Props> = ({todos, setTodos}: Props) => {
-  return (
-    <div className='todos'>
-        { todos.map((todo) => (
-            
-            <SingleTodo //passing props to SingleTodo
-                todo={todo} 
-                key={todo.id}
-                todos={todos}
-                setTodos={setTodos}
-                />
-            
-        ))}
-     </div>
-  )
+const TodoList: React.FC<Props> = ({ todos, setTodos }: Props) => {
+    return (
+
+        <div className='todo-container'>
+            {/* dropable zone */}
+            <Droppable droppableId='TodosList'> 
+                {
+                    (provided) => (
+                        // active todo section
+                        <div className='todos' ref={provided.innerRef} {...provided.droppableProps}>
+                            <span className="todos-heading"> Active tasks </span>
+                            {todos.map((todo) => (
+                            <SingleTodo
+                                todo={todo}
+                                todos={todos}
+                                key={todo.id}
+                                setTodos={setTodos}
+                             />
+                            ))}
+                        </div>
+
+                )}
+            </Droppable >
+
+
+    
+    < div className = 'todos finished' >
+        <span className="todos-heading">
+            Completed tasks
+        </span>
+{
+    todos.map((todo) => (
+        <SingleTodo
+            todo={todo}
+            todos={todos}
+            key={todo.id}
+            setTodos={setTodos} />
+    ))
+}
+            </ >
+        </div >
+    )
 }
 
 export default TodoList
